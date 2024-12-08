305ce95746b1e94bc374e603b0be70a497995bd2
GonzaloRojas <gonzalo.edurojas@gmail.com>
2024-12-08 16:06:48 -0300

    funcion para agregar div presionando teclas

--- assets/js/script4colores.js
+++ assets/js/script4colores.js
@@ -6,32 +6,16 @@ const divRojo = document.getElementById('rojo')
 const divVerde = document.getElementById('verde')
 const divAmarillo = document.getElementById('amarillo')
 
-const colorNegro = function(colorDiv, color) {
+const colorNegro = function (colorDiv, color) {
     colorDiv.addEventListener('click', () => {
         colorDiv.style.backgroundColor = color
     })
-} 
+}
 colorNegro(divAzul, 'black')
 colorNegro(divRojo, 'black')
 colorNegro(divVerde, 'black')
 colorNegro(divAmarillo, 'black')
 
-/*divAzul.addEventListener('click', () => {
-    divAzul.style.backgroundColor = 'black'
-})
-
-divRojo.addEventListener('click', () => {
-    divRojo.style.backgroundColor = 'black'
-})
-
-divVerde.addEventListener('click', () => {
-    divVerde.style.backgroundColor = 'black'
-})
-
-divAmarillo.addEventListener('click', () => {
-    divAmarillo.style.backgroundColor = 'black'
-})
-*/
 /*CAMBIAR COLOR PRESIONANDO A,S,D*/
 
 const divKey = document.getElementById('key')
@@ -40,33 +24,37 @@ document.addEventListener('keydown', (event) => {
     if (event.key === 'a') {
         divKey.style.backgroundColor = 'pink'
     }
-    else if (event.key === 's'){
+    else if (event.key === 's') {
         divKey.style.backgroundColor = 'orange'
     }
 
-    else if (event.key === 'd'){
+    else if (event.key === 'd') {
         divKey.style.backgroundColor = 'skyBlue'
     }
 })
 
 /*AGREGAR DIV PRESIONANDO Q,W,E*/
 
-document.addEventListener('keydown', (event) => {
-let colorNewDiv = false
+const createDiv = function (tecla, color) {
+    document.addEventListener('keydown', (event) => {
+        let colorNewDiv = false
 
-if (event.key=== 'q') {
-    colorNewDiv = 'purple'
-} else if (event.key === 'w'){
-    colorNewDiv = 'grey'
-} else if (event.key === 'e') {
-    colorNewDiv = 'brown'
+        if (event.key === tecla) {
+            colorNewDiv = color
+        }
+
+        if (colorNewDiv) {
+            const newDiv = document.createElement('div')
+            newDiv.style.width = '200px'
+            newDiv.style.height = '200px'
+            newDiv.style.backgroundColor = colorNewDiv
+            newDiv.style.margin = '10px'
+            document.body.appendChild(newDiv)
+        }
+
+    })
 }
 
-if (colorNewDiv){
-    const newDiv = document.createElement('div')
-    newDiv.style.width = '200px'
-    newDiv.style.height = '200px'
-    newDiv.style.backgroundColor = colorNewDiv
-    newDiv.style.margin = '10px'
-    document.body.appendChild(newDiv)}
-}) 
+createDiv('q', 'purple')
+createDiv('w', 'grey')
+createDiv('e', 'brown')
